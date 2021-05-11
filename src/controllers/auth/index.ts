f3317010a64d4cc33883de3764e8882a7606f3e3
Diego Herrero <diegodhh@gmail.com>
2021-05-11 19:29:01 -0300

    refresh token and redisi2

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,5 +1,5 @@
-import { loginController } from "./loginController";
-import { refreshTokenController } from "./refreshTokenController";
-import { signUpController } from "./signUpController";
+import { loginController } from "./login.controller";
+import { refreshTokenController } from "./refreshToken.controller";
+import { signUpController } from "./signUp.controller";
 
 export { loginController, signUpController, refreshTokenController };
